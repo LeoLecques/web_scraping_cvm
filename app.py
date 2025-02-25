09ee10238d34537833f0b65242672ec546c0aca8
LeoLecques <leonardolecquespos@gmail.com>
2025-02-25 00:37:25 -0300

    Atualizacao de graficos

--- app.py
+++ app.py
@@ -1,5 +1,10 @@
-from flask import Flask, render_template, request, redirect, url_for, flash
+from flask import Flask, render_template, request, redirect, url_for, flash,jsonify
 import sqlite3
+import pandas as pd
+import plotly.express as px
+import plotly
+import json
+import os
 
 app = Flask(__name__)
 app.secret_key = 'senha_teste' 
@@ -14,6 +19,14 @@ def get_table_names():
     cursor = conn.cursor()
     cursor.execute("SELECT name FROM sqlite_master WHERE type='table';")
     table_names = [row['name'] for row in cursor.fetchall()]
+    conn.close()
+    return table_names
+
+def get_table_names_dash():
+    conn = get_db_connection()
+    cursor = conn.cursor()
+    cursor.execute("SELECT name FROM sqlite_master WHERE type='table';")
+    table_names = {row['name']: format_table_name(row['name']) for row in cursor.fetchall()}
     conn.close()
     return table_names
 
@@ -76,6 +89,56 @@ def delete_row(table_name, id):
     flash('Registro deletado com sucesso!', 'success')
     return redirect(url_for('view_table', table_name=table_name))
 
+
+def generate_plot(year, table, categories, title):
+    conn = get_db_connection()
+    query = f"SELECT * FROM {table}_{year}"
+    df = pd.read_sql_query(query, conn)
+    df_melt = df[categories].sum().reset_index()
+    df_melt.columns = ['Categoria', 'Quantidade']
+    fig = px.pie(df_melt, values='Quantidade', names='Categoria', title=title)
+    plot_json = json.dumps(fig, cls=plotly.utils.PlotlyJSONEncoder) 
+    conn.close()
+    return plot_json
+
+@app.route('/dashboards', methods=['GET'])
+def dashboards():
+    year = request.args.get('year', '2025')
+    table_names = get_table_names_dash()
+
+    plots = {
+        'race': generate_plot(year, 'fre_cia_aberta_empregado_local_declaracao_raca', 
+                              ['Quantidade_Amarelo', 'Quantidade_Branco', 'Quantidade_Preto', 'Quantidade_Pardo', 'Quantidade_Indigena', 'Quantidade_Outros', 'Quantidade_Sem_Resposta'], 
+                              'Distribuição Percentual por Raça'),
+        'gender': generate_plot(year, 'fre_cia_aberta_empregado_local_declaracao_genero',
+                                ['Quantidade_Feminino', 'Quantidade_Masculino', 'Quantidade_Nao_Binario', 'Quantidade_Outros', 'Quantidade_Sem_Resposta'], 
+                                'Distribuição Percentual por Gênero'),
+        'age': generate_plot(year, 'fre_cia_aberta_empregado_local_faixa_etaria',
+                             ['Quantidade_Ate30Anos', 'Quantidade_30a50Anos', 'Quantidade_Acima50Anos'], 
+                             'Distribuição Percentual por Faixa Etária')
+    }
+    return render_template('dashboards.html', plots=plots, year=year, table_names=table_names)
+
+
+@app.route('/get_dashboard_data', methods=['GET'])
+def get_dashboard_data():
+    year = request.args.get('year', '2025')
+
+    try:
+        plots = {
+            'race': generate_plot(year, 'fre_cia_aberta_empregado_local_declaracao_raca', 
+                                  ['Quantidade_Amarelo', 'Quantidade_Branco', 'Quantidade_Preto', 'Quantidade_Pardo', 'Quantidade_Indigena', 'Quantidade_Outros', 'Quantidade_Sem_Resposta'], 
+                                  'Distribuição Percentual por Raça'),
+            'gender': generate_plot(year, 'fre_cia_aberta_empregado_local_declaracao_genero',
+                                    ['Quantidade_Feminino', 'Quantidade_Masculino', 'Quantidade_Nao_Binario', 'Quantidade_Outros', 'Quantidade_Sem_Resposta'], 
+                                    'Distribuição Percentual por Gênero'),
+            'age': generate_plot(year, 'fre_cia_aberta_empregado_local_faixa_etaria',
+                                 ['Quantidade_Ate30Anos', 'Quantidade_30a50Anos', 'Quantidade_Acima50Anos'], 
+                                 'Distribuição Percentual por Faixa Etária')
+        }
+        return jsonify(plots)
+    except Exception as e:
+        return jsonify({"error": str(e)}), 500
+
 if __name__ == '__main__':
     app.run(debug=True)
-
